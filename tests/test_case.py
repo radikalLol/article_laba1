4410b3e9748d4ed560d4d37396de548a3cdfaa1c
radik <radikal.xxxxx@gmail.com>
2019-09-29 22:53:18 +0300

    q
    q

--- tests/test_case.py
+++ tests/test_case.py
@@ -2,7 +2,7 @@ import pytest
 from config import TestingConfig, Config
 from app import app, db
 
-def test_test_conf(app):
+def test_test_conf():
     app.config.from_object(TestingConfig)
     assert app.config['DEBUG']
     assert app.config['SQLALCHEMY_DATABASE_URI']
