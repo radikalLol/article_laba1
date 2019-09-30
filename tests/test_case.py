d0ccd6842213b292ec9f6a87f720de52874db248
radik <radikal.xxxxx@gmail.com>
2019-09-30 12:44:32 +0300

    qq
    q

--- tests/test_case.py
+++ tests/test_case.py
@@ -15,3 +15,17 @@ def test_index():
     response = client.get('/')
     assert response.status_code == 200
     assert b'Hello World!' in response.data
+
+def test_db():
+    with app.app_context() as context:
+        context.push()
+        db.drop_all()
+        db.create_all()
+
+        from models import Articles
+
+        test_art = Articles(name="article article", author = "author article", published= "13.04.2001")
+        test_art.save()
+        db.session.commit()
+
+    assert db.session.query(Articles).one()
