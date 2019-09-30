f8233ec9fc30143afc5d14ebf72976696484110a
radik <radikal.xxxxx@gmail.com>
2019-09-30 11:26:00 +0300

    3
    3

--- tests/test_case.py
+++ tests/test_case.py
@@ -8,3 +8,10 @@ def test_test_conf():
     assert app.config['DEBUG']
     assert app.config['SQLALCHEMY_DATABASE_URI']
     assert app.config['TESTING']
+
+
+def test_index():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Hello World!' in response.data
