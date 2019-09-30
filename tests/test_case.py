81d37943444346eeb679c811b339b197dc973b91
radikalLol <radikal.xxxxx@gmail.com>
2019-09-30 20:26:04 +0300

    S
    1

--- tests/test_case.py
+++ tests/test_case.py
@@ -3,14 +3,14 @@ from config import TestingConfig, Config
 from app import app, db
 
 
-def test_test_conf():
+def test_conf():
     app.config.from_object(TestingConfig)
     assert app.config['DEBUG']
     assert app.config['SQLALCHEMY_DATABASE_URI']
     assert app.config['TESTING']
 
 
-def test_index():
+def test_hello():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
